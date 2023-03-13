0ed75268df7953517ed82f481c0a6c3531222334
hisoq <khuksel@mail.ru>
2023-03-13 21:58:19 +0100

    first Two (color and 16 phones) 3 dobavil index + views

--- myapp/telefon/models.py
+++ myapp/telefon/models.py
@@ -5,7 +5,7 @@ class Phones(models.Model):
     brand = models.ForeignKey('Brand', on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=50, verbose_name='Модель')
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
-    price = models.FloatField(null=True, blank=True, verbose_name='Цена')
+    price = models.IntegerField(null=True, blank=True, verbose_name='Цена в $')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата публикации')
     color = models.CharField(max_length=20, default='black', verbose_name='Цвет')
 
@@ -18,7 +18,7 @@ class Phones(models.Model):
         ordering = ('price',)
 
     def __str__(self):
-        return f'{self.title}'
+        return f'{self.title} - {self.price}'
 
 class Brand(models.Model):
     name = models.CharField(max_length=10)
--- myapp/telefon/views.py
+++ myapp/telefon/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.http import HttpResponse
+from .models import Phones
 
 def index(request):
-    return HttpResponse("phone")
+    pph = Phones.objects.order_by('-published')
+    return render(request, 'myapp/index.html', {'pph': pph})
+    # s = 'Список объявлний\r\n\r\n\r\n'
+    # for i in Phones.objects.order_by('-published'):
+    #     s += Phones.title + '\r\n' + Phones.content + '\r\n\r\n'
+    #return HttpResponse('PHONE')
